77db5b455b0c84e03b352879b367520e76329120
kwansai724 <kwansai.taka724@gmail.com>
2023-12-06 22:14:46 +0900

    MUI, emotion追加

--- next/next.config.js
+++ next/next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Syntax error: "next/font" requires SWC although Babel is being used due to a custom babel config being present.
+  // swcMinify: false, // 上記エラーが発生したらコメントアウト外す
 }
 
 module.exports = nextConfig
